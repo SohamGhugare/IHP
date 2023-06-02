4b803e5402c3ab75738b1d95c0f50fd1561360e4
MehakK1311 <mehakhanna1311@gmail.com>
2023-06-03 00:20:59 +0530

    feat: user login

--- src/components/Auth/Patient/Login.jsx
+++ src/components/Auth/Patient/Login.jsx
@@ -1,6 +1,6 @@
-import { Button,Typography, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { Button, Typography, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 
-const MyForm = () => {
+const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission
@@ -8,46 +8,50 @@ const MyForm = () => {
 
   return (
 
-    <div className="flex items-center justify-center ">
-      <form onSubmit={handleSubmit} className="w-full max-w-md">
-        <Typography variant="h2" className="text-2xl font-bold mb-6">
-          Login
-        </Typography>
+    <div className="flex items-center justify-center h-screen">
+      <form className="w-full max-w-md">
+        <h2 className="text-5xl font-bold mb-6">Welcome Back</h2>
 
-        <TextField
-          id="fullName"
-          label="Full Name"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          required
-          InputLabelProps={{
-            style: { color: '#838383' },
-          }}
-        />
+        <div className="mb-4">
+          <label htmlFor="fullName" className="block mb-2">
+            IHP ID Number:
+          </label>
+          <input
+            type="number"
+            id="fullName"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-md"
+            required
+          />
+        </div>
 
-        <TextField
-          id="phoneNumber"
-          label="Phone Number"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          required
-          InputLabelProps={{
-            style: { color: '#838383' },
-          }}
-        />
-
-        {/* Rest of the input fields with customized label color */}
-
-        <Button
+        <div className="mb-4">
+          <label htmlFor="phoneNumber" className="block mb-2">
+            Phone Number:
+          </label>
+          <input
+            type="tel"
+            id="phoneNumber"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-md"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="fullName" className="block mb-2">
+            Aadhaar Number
+          </label>
+          <input
+            type="text"
+            id="fullName"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-md"
+            required
+          />
+        </div>
+        <button
           type="submit"
-          variant="contained"
-          color="primary"
-          fullWidth
+          className="bg-blue-500 text-white px-4 py-2 rounded-xl mx-auto w-full"
         >
           Login
-        </Button>
+        </button>
       </form>
     </div>
 
@@ -56,4 +60,4 @@ const MyForm = () => {
   );
 };
 
-export default MyForm;
+export default Login;
