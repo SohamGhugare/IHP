2728b3668527ec229c7c9042bc811103f110f646
SohamGhugare <soham.ghugare@gmail.com>
2023-06-02 21:02:24 +0530

    feat: integrated file storage

--- backend/ipfs/ipfsHandler.go
+++ backend/ipfs/ipfsHandler.go
@@ -1,9 +1,8 @@
 package ipfs
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/SohamGhugare/IHP/initializers"
@@ -14,19 +13,17 @@ type User struct {
 	Name string `json:"name"`
 }
 
-func StoreFile() {
-	user := &User{
-		IHP:  12345,
-		Name: "Soham Ghugare",
+func StoreFile(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("error while storing file: %v", err.Error())
 	}
+	defer file.Close()
 
-	userBin, _ := json.Marshal(user)
-	reader := bytes.NewReader(userBin)
-
-	cid, err := initializers.Sh.Add(reader)
+	cid, err := initializers.Sh.Add(file)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s", err)
 		os.Exit(1)
 	}
-	fmt.Printf("added %s\n", cid)
+	fmt.Printf("Added file with CID %s\n", cid)
 }
--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-	"github.com/SohamGhugare/IHP/contract"
 	"github.com/SohamGhugare/IHP/initializers"
 	"github.com/SohamGhugare/IHP/ipfs"
 	"github.com/gin-gonic/gin"
 )
 
 func init() {
-	contract.ConnectContract()
+	// contract.ConnectContract()
 	initializers.LoadEnvVars()
 	initializers.ConnectIPFS()
 }
 
 func main() {
 
-	ipfs.StoreFile()
+	ipfs.StoreFile("dummy/test.png")
 
 	r := gin.Default()
 
