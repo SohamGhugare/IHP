0d5a36f4da07fae7f0b1e734d6f467f62959b3d3
MehakK1311 <mehakhanna1311@gmail.com>
2023-06-04 01:57:18 +0530

    fix: doc profile

--- src/pages/doctor/Profile.jsx
+++ src/pages/doctor/Profile.jsx
@@ -4,16 +4,23 @@ import Banner from '../../components/Doctor/Banner'
 import Searchbar from '../../components/Doctor/Searchbar'
 import About from '../../components/Doctor/About'
 import { BsQrCodeScan } from "react-icons/bs";
+import { Link } from 'react-router-dom'
+import Experience from '../../components/Doctor/Experience'
+import Awards from '../../components/Doctor/Awards'
+import Video from '../../components/Doctor/Video'
+import Footer from '../../components/Doctor/Footer'
 
 const Profile = () => {
   return (
     <div className="flex flex-col justify-between ">
       <Header />
       <Banner />
-      <div class="fixed bottom-6 right-6">
-        <button class="bg-blue-500 hover:bg-blue-600 text-white w-16 h-16 rounded-full flex items-center justify-center">
-          <BsQrCodeScan className="w-6 h-6"/>
-        </button>
+      <div class="fixed bottom-20 right-6">
+        <Link to="https://sourceai.in/qrscan">
+          <button class="bg-blue-500 hover:bg-blue-600 text-white w-16 h-16 rounded-full flex items-center justify-center">
+            <BsQrCodeScan className="w-6 h-6" />
+          </button>
+        </Link>
       </div>
       <Searchbar />
       {/* <div>
@@ -23,6 +30,12 @@ const Profile = () => {
       <div className="mt-10">
         <About />
       </div>
+      
+        <Experience/>
+        <Awards/>
+        <Video/>
+        <Footer/>
+      
     </div>
   )
 }
--- src/pages/patient/ProfileUser.jsx
+++ src/pages/patient/ProfileUser.jsx
@@ -4,6 +4,7 @@ import Banner from '../../components/Patient/Banner'
 import TabPanel from '../../components/Patient/TabPanel'
 import Searchbar from '../../components/Patient/Searchbar'
 import TextHead from '../../components/Patient/TextHead'
+import Footer from '../../components/Patient/Footer'
 
 const ProfileUser = () => {
   return (
@@ -24,6 +25,7 @@ const ProfileUser = () => {
       <div className="mt-10">
         <TabPanel/>
       </div> */}
+      <Footer/>
     </div>
   )
 }
